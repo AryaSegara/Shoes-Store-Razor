18b56e68eaf485dc6e2f402e0b910367ca430eb8
Arya Segara <aryasegara10@gmail.com>
2025-08-21 10:02:38 +0700

    update

--- Shoes-Store/obj/Debug/net8.0/Shoes-Store.AssemblyInfo.cs
+++ Shoes-Store/obj/Debug/net8.0/Shoes-Store.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Shoes-Store")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+690062da3bc0b14bae63512cbbafe51b1e9baba0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08cb3d1645be7af01df7b5b6c3b91f28dc17452f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Shoes-Store")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Shoes-Store")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
